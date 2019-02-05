bd60e48378d772b9d36a2fb71642083d0dd89c76
Harshith46 <Harshith@quintype.com>
2019-02-05 17:03:10 +0530

    added answer for 3rd and 4th question

--- lifeCycleOfObjectExercises.js
+++ lifeCycleOfObjectExercises.js
@@ -42,7 +42,55 @@
     group.add(i);
     } return group;
   }
+}
+
+3)class Group {
+  
+  constructor() {
+    this.arr1=[];
+  }
+  delete(value){
+    this.arr1=this.arr1.filter((a)=> a!==value);
+  }
+  has(value){
+    return this.arr1.includes(value);
+  }
+  
+  add(value){
+    if(!this.has(value)){
+    this.arr1.push(value);
+    }
+  }
+   
+  static from(arr){
+    let group=new Group;
+    for(let i of arr) {
+    group.add(i);
+    } return group;
+  }
+  
+  [Symbol.iterator]() {return new GroupIterator(this)};
 
 }
+
+class GroupIterator{
+  constructor(group){
+    this.group=group;
+    this.i=0;
+  }
+  
+  next(){
+    if(this.i>=this.group.length){
+    return {done: true};
+    } else {
+      let res1= {value: this.group[this.i], done: false};
+      this.i++;
+      return res1;
+    }
+  }
+}
+
+4)let map = {one: true, two: true, hasOwnProperty: true};
+console.log(Object.prototype.hasOwnProperty.call(map,"one"));
 
 
